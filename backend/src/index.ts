a904cbd35eb71149cbeb14eaaa67e9f36042167c
Rigoberto Rosa <rigoros@amazon.com>
2023-09-17 13:17:13 -0400

    Add backend number guessing api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express, { Express } from 'express';
 import { babyNameRouter } from './homework-2/routes/baby-name-routes';
 import { ROUTES } from './utils/server-constants';
+import { numberGuessingRoute } from './homework-2/routes/number-guessing-routes';
 
 const PORT = process.env.PORT ?? 8080;
 const app: Express = express();
@@ -9,6 +10,7 @@ const app: Express = express();
 app.use(express.json());
 
 app.use(ROUTES.BABY_NAME_BASE_ROUTE, babyNameRouter);
+app.use(ROUTES.BASE_ROUTE, numberGuessingRoute);
 
 app.listen(PORT, () => {
   console.log(`PORT LISTENING ${PORT}`);
